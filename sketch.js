93c9ca53066a6f79fce57fb8c74ce7b38e128368
PratikshyaPradhan <69904528+PratikshyaPradhan@users.noreply.github.com>
2020-10-31 10:16:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock;
+var Dog1, DogHappy;
+var gameState;
+var foodType = null;
+var happiness = 0;
+var fedTime, lastFed;
+var feed, addFood;
+var foodObj;
+
+function preload()
+{
+  //load images here
+  Dog1 = loadImage("images/dogImg.png");
+  DogHappy = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(1000, 400);
+
+  foodObj = new Milk();
+
+  foodStock = database.ref('food');
+  foodStock.on("value", readStock);
+
+  dog = createSprite(800, 200, 150, 150);
+  dog.addImage(Dog1);
+  dog.scale = 0.15;
+
+  feed = createButton("Feed the Dog");
+  feed.position(700, 95);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("Add Food");
+  addFood.position(800, 95);
+  addFood.mousePressed(addFoods);
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+  foodObj.display();
+   
+  fedTime = database.ref('FeedTime');
+  fedTime.on("value", function(data){
+    lastFed = data.val();
+  });
+
+  fill(255, 255, 254);
+  textSize(15);
+  if(lastFed >=12){
+    text("Last Fed: "+lastFed%12 + " PM", 350 ,30);
+  }
+  else if(lastFed === 0){
+    text("Last Fed: 12 AM", 350 ,30);
+  }
+  else{
+    text("Last Fed: "+lastFed%12 + " AM", 350 ,30);
+  }
+
+  drawSprites();
+}
+
+function feedDog(){
+  dog.addImage(DogHappy);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    food : foodObj.getFoodStock(),
+    FeedTime: hour()
+  })
+}
+
+function readStock(data){
+  foodS = data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0; 
+  }
+  else{
+    x -=1;
+  }
+
+  database.ref('/').update({
+    food:x
+  })
+}
+
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    food:foodS
+  })
+}
+
+function keyPressed(){
+  if(keyCode === 32 && foodS == 0){
+    database.ref('/').update({
+      food: 20
+    })
+  }
+}
